3d79bde341554526f7e767761424c4c914aebad4
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 12:23:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -7,13 +7,13 @@ end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
-  binding.pry
+ # binding.pry
 end
 
 def engage
   puts state_log(date)
   date = generate_star_date
-  binding.pry
+ # binding.pry
 end
 
 
